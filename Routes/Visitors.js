a49159ebf3e61f8ff17f0d004c4e954cc84ec043
Prashant Keshav <63390386+theprashantkeshav@users.noreply.github.com>
2023-04-25 07:59:35 +0530

    second update

--- Routes/Visitors.js
+++ Routes/Visitors.js
@@ -3,23 +3,24 @@ import express from "express";
 
 const router = express.Router();
 
-const visitorRoute = router.put("/visitors/:id", (req, res) => {
+const visitorRoute = router.post("/visitors/", (req, res) => {
   const data = req.body;
-  const id = req.params.id;
-
-  const update = {
+  const datas = new First({
+    url: data.url,
+    referrer: data.referrer,
     firstName: data.firstName,
     lastName: data.lastName,
     email: data.email,
     dob: new Date(data.dob),
     number: data.number,
-  };
-  First.findByIdAndUpdate(id, update, (err) => {
+  });
+
+  datas.save((err, doc) => {
     if (err) {
       console.error(err);
-      res.status(500).send("ErrorS2...");
+      res.status(500).send({ error: "Error saving data to database" });
     } else {
-      res.send("SavedS2...");
+      res.send({ _id: doc._id });
     }
   });
 });
