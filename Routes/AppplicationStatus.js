16f57f6286c9d613e303dc681dec0dc00615d1d8
mattb123123 <110488008+mattb123123@users.noreply.github.com>
2023-10-06 08:22:39 -0400

    Add files via upload

---
+++ Routes/AppplicationStatus.js
@@ -0,0 +1,22 @@
+import First from "../Models/First";
+import express from "express";
+
+const router = express.Router()
+
+const ApplicationStatus = router.put("/application-status/:id", (req, res) => {
+    const data = req.body;
+    const id = req.param.id;
+    const update = {
+        applicationStatus: data.applicationStatus,
+    }
+    First.findByIdAndUpdate(id, update, (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error4b...");
+        } else {
+          res.send("Saved4b...");
+        }
+      });
+})
+
+export default ApplicationStatus;
